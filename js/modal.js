1f8027164dd2eb404764d60ff70fd2588e9f2259
Ivar Hernández <ivar.hdez@gmail.com>
2016-06-16 10:41:19 -0500

    Home v 1.3
    Incluye Slide y Modal

---
+++ js/modal.js
@@ -0,0 +1,21 @@
+  /*
+* This is the plugin
+*/
+(function(a){a.createModal=function(b){defaults={title:"",message:"Your Message Goes Here!",closeButton:true,scrollable:false};var b=a.extend({},defaults,b);var c=(b.scrollable===true)?'style="max-height: 500px;overflow-y: auto;"':"";html='<div class="modal fade" id="myModal">';html+='<div class="modal-dialog">';html+='<div class="modal-content">';html+='<div class="modal-body" '+c+">";html+=b.message;html+="</div>";html+='<div class="modal-footer">';if(b.closeButton===true){html+='<button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>'}html+="</div>";html+="</div>";html+="</div>";html+="</div>";a("body").prepend(html);a("#myModal").modal().on("hidden.bs.modal",function(){a(this).remove()})}})(jQuery);
+
+/*
+* Here is how you use it
+*/
+$(function(){    
+    $('.view-pdf').on('click',function(){
+        var pdf_link = $(this).attr('href');
+        //var iframe = '<div class="iframe-container"><iframe src="'+pdf_link+'"></iframe></div>'
+        //var iframe = '<object data="'+pdf_link+'" type="application/pdf"><embed src="'+pdf_link+'" type="application/pdf" /></object>'        
+        var iframe = '<object type="application/pdf" data="'+pdf_link+'" width="100%" height="500px">No Support</object>'
+        $.createModal({
+            message: iframe,
+            scrollable:false
+        });
+        return false;        
+    });    
+})
