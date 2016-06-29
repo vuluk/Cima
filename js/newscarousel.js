7322b17ef0da6e14025e02f2b5ad8f7171527f3e
Ivar Hernández <ivar.hdez@gmail.com>
2016-06-29 17:36:22 -0500

    JS Carrusel
    Va

--- js/newscarousel.js
+++ js/newscarousel.js
@@ -1,24 +1,31 @@
-    $(document).ready(function(){
-    
-    var clickEvent = false;
-    $('#myCarousel').carousel({
-        interval:   4000    
-    }).on('click', '.list-group li', function() {
-            clickEvent = true;
-            $('.list-group li').removeClass('active');
-            $(this).addClass('active');     
-    }).on('slid.bs.carousel', function(e) {
-        if(!clickEvent) {
-            var count = $('.list-group').children().length -1;
-            var current = $('.list-group li.active');
-            current.removeClass('active').next().addClass('active');
-            var id = parseInt(current.data('slide-to'));
-            if(count == id) {
-                $('.list-group li').first().addClass('active'); 
-            }
-        }
-        clickEvent = false;
-    });
+$(document).ready(function(){
+  var clickEvent = false;
+  $('#myCarousel').carousel({
+      interval:   4000
+  }).on('click', '.list-group li', function() {
+          clickEvent = true;
+          $('.list-group li').removeClass('active');
+          $(this).addClass('active');
+  }).on('mouseover', '.list-group li', function() {
+          clickEvent = true;
+          $('.list-group li').removeClass('active');
+          $(this).addClass('active');
+          var id = parseInt($(this).data('slide-to'));
+          $(this).carousel(id);
+  }).on('mouseover', '.carousel-inner', function() {
+          $(this).carousel('pause');
+  }).on('slid.bs.carousel', function(e) {
+      if(!clickEvent) {
+          var count = $('.list-group').children().length -1;
+          var current = $('.list-group li.active');
+          current.removeClass('active').next().addClass('active');
+          var id = parseInt(current.data('slide-to'));
+          if(count == id) {
+              $('.list-group li').first().addClass('active');
+          }
+      }
+      clickEvent = false;
+  });
 })
 
 $(window).load(function() {
